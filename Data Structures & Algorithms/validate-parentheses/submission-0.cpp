ac6832d63f42fa89b10d6a803f4ab3745943cf45
Asmit Yadav <114602189+asmit-yadav@users.noreply.github.com>
2026-04-08 12:10:07 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+      stack<char>st;
+      
+      for(int i=0;i<s.size();i++)
+      {
+        char ch=s[i];
+        if(ch=='(' || ch =='[' || ch=='{')
+        {
+            st.push(ch);
+        }
+        else
+        {
+            if(st.empty()==true)
+            return false;
+            else if((ch==')' && st.top()=='(') || (ch==']' && st.top()=='[') ||( ch=='}' && st.top()=='{'))
+            {
+                st.pop();
+            }
+            else 
+            return false;
+        }
+      }
+
+      return (st.empty());
+    }
+};
